6b285081ea59d6b3206c2a93b549c15be4babd5b
Kmxxx24 <122954922+Kmxxx24@users.noreply.github.com>
2023-03-16 10:53:50 +0700

    Initial commit

---
+++ model/course.php
@@ -0,0 +1,35 @@
+<!-- Course section -->
+<?php 
+$cs_img="p1.png";
+$cs_name="SAMSUNG";
+$cs_screen="6.8นิ้ว จอ Dynamic AMOLED 2X 24bit 1440*3088 พิทเซล";
+$cs_camera="200?ญ +10MP กล้องหน้า 12 MP";
+$cs_chip="ความเร็ว 3.2 GHz";
+$cs_UI="one UI 5.1 based on Android 13";
+$cs_memory="RAM 8/12 GB ROM 256/51 GB ";
+$cs_battery="5,000mAh ชาร์จไว 45 w";
+?>
+<div class="col mb-5">
+                    <a href="course/12.html" class="card-link">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="<?=$cs_img;?>" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-left">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder course-name"><?=$cs_name;?></h5>
+                                    <!-- course tag line-->
+                                    <div class="course-note"><i class="far fa-calendar-alt"></i>  <?=$cs_screen;?></div>
+                                    <!-- <div class="course-note"><i class="fab fa-internet-explorer"></i> -</div> -->
+                                    <div class="course-note"><i class="far fa-building"></i> </div>
+                                    <!-- <div class="course-note"><i class="fas fa-graduation-cap"></i> - </div> -->
+                                    <div class="course-note"><i class="fas fa-wallet"></i> <?=$cs_camera;?></div>
+
+                                </div>
+                            </div>
+                        </div>
+                      </a>
+                  </div>
+                  <!-- End Course section -->
+                
